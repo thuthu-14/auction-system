ec44350367845fbe79deb7dc561db2be16b456a1
nhipham-cd <25023352@vnu.edu.vn>
2026-04-15 16:40:03 +0700

    wip: home-bidder + home-seller UI

---
+++ src/main/java/io/auctionsystem/controllers/ManageAuctionsController.java
@@ -0,0 +1,30 @@
+package io.auctionsystem.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class ManageAuctionsController {
+
+    @FXML
+    private TableView<?> auctionTable;
+
+    @FXML
+    private void handleCreateAuction() {
+        System.out.println("Tạo phiên mới");
+    }
+
+    @FXML
+    private void handleDeleteAuction() {
+        System.out.println("Xóa phiên");
+    }
+
+    @FXML
+    private void handleViewHistory() {
+        System.out.println("Xem bid history");
+    }
+
+    @FXML
+    private void handleRefresh() {
+        System.out.println("Refresh danh sách");
+    }
+}
