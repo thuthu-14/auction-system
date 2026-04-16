5ff170480fc3b7741ee133d53fbc77c905522e54
Dinh Ha Ngan <25023336@vnu.edu.vn>
2026-04-16 23:49:23 +0700

    add new file

---
+++ source/src/main/java/server/service/AuctionService.java
@@ -0,0 +1,26 @@
+public class AuctionService {
+
+    public static Auction createAuction(RegularUser seller, Item item, int durationMinutes)
+            throws PermissionDeniedException, IOException, ClassNotFoundException {
+
+        // TODO: validate seller and item
+        // TODO: validate price and duration
+        // TODO: create auction
+        return null;
+    }
+
+    public static List<Auction> getActiveAuctions()
+            throws IOException, ClassNotFoundException {
+        return null;
+    }
+
+    public static Auction getAuctionById(String auctionId)
+            throws IOException, ClassNotFoundException {
+        return null;
+    }
+
+    public static void finishAuction(String auctionId)
+            throws IOException, ClassNotFoundException {
+        // TODO
+    }
+}
