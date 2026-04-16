f09d5968339488fb0ef2c27610cfc3211014710f
thuthu-14 <thu07114@thu07114gmail.07114gmailcom>
2026-04-16 23:03:28 +0700

    add common

---
+++ source/src/main/java/common/UserRole.java
@@ -0,0 +1,19 @@
+package common;
+
+import java.io.Serializable;
+
+public enum UserRole implements Serializable {
+    BIDDER("Người mua - Tham gia đấu giá"),
+    SELLER("Người bán - Tạo phiên đấu giá"),
+    ADMIN("Quản trị viên - Toàn quyền quản lý");
+
+    private final String description;
+
+    UserRole(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
