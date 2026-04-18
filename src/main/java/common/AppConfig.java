6ab8d9b243477866d3eec339c319f2d73aabaeca
nhipham-cd <25023352@vnu.edu.vn>
2026-04-19 02:15:23 +0700

    add 2 file common + wip: Home.fxml

---
+++ src/main/java/common/AppConfig.java
@@ -0,0 +1,51 @@
+// src/main/java/common/AppConfig.java
+package common;
+
+/**
+ * AppConfig - Cấu hình ứng dụng
+ * Design Pattern: Singleton
+ */
+public class AppConfig {
+
+    private static String serverHost = Constants.SERVER_HOST;
+    private static int serverPort = Constants.SERVER_PORT;
+    private boolean isDebugMode = false;
+    private static AppConfig instance;
+
+    private AppConfig() {}
+
+    public static synchronized AppConfig getInstance() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    public String getServerHost() {
+        return serverHost;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public boolean isDebugMode() {
+        return isDebugMode;
+    }
+
+    public void setServerHost(String host) {
+        serverHost = host;
+    }
+
+    public void setServerPort(int port) {
+        serverPort = port;
+    }
+
+    public void setDebugMode(boolean debug) {
+        isDebugMode = debug;
+    }
+
+    public String getServerURL() {
+        return "http://" + serverHost + ":" + serverPort;
+    }
+}
