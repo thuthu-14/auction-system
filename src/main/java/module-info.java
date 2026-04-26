0a0b3cfa5814de0a68f7c9529daa1839163a1be9
bhbngoc146 <25023341@vnu.edu.vn>
2026-04-26 10:41:57 +0700

    refactor: Chuẩn hóa lại cấu trúc thư mục Maven

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module io.auctionsystem {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens io.auctionsystem.controllers to javafx.fxml, javafx.graphics;
-
-    exports io.auctionsystem.controllers;
-}
