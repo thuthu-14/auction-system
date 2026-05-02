8f51862ccbb1e3d0a9951371391cb774db3b06e7
thuthu-14 <thu07114@thu07114gmail.07114gmailcom>
2026-05-03 01:01:06 +0700

    refactor(storage): simplify AuctionDAO by using JSON as primary storage and improving null safety

--- src/main/java/server/storage/AuctionDAO.java
+++ src/main/java/server/storage/AuctionDAO.java
@@ -3,19 +3,24 @@ package server.storage;
 import server.model.Auction;
 import common.AuctionStatus;
 import util.JsonUtil;
-import util.SerializationUtil;
 import util.LoggerUtil;
 import java.io.IOException;
 import java.util.*;
 
 public class AuctionDAO {
 
+    /**
+     * Lấy tất cả danh sách đấu giá từ file JSON
+     */
     public static List<Auction> getAllAuctions() throws IOException, ClassNotFoundException {
         JsonUtil.createFileIfNotExists(DataManager.JSON_AUCTIONS);
         List<Auction> auctions = JsonUtil.loadListFromJson(DataManager.JSON_AUCTIONS, Auction.class);
         return auctions != null ? auctions : new ArrayList<>();
     }
 
+    /**
+     * Tìm một phiên đấu giá theo ID
+     */
     public static Auction getAuctionById(String auctionId) throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
         for (Auction auction : auctions) {
@@ -26,17 +31,23 @@ public class AuctionDAO {
         return null;
     }
 
+    /**
+     * Lấy danh sách đấu giá của một Seller cụ thể
+     */
     public static List<Auction> getAuctionsBySellerId(String sellerId) throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
         List<Auction> result = new ArrayList<>();
         for (Auction auction : auctions) {
-            if (auction.getSellerId().equals(sellerId)) {
+            if (auction.getSellerId() != null && auction.getSellerId().equals(sellerId)) {
                 result.add(auction);
             }
         }
         return result;
     }
 
+    /**
+     * Lấy danh sách các phiên đang mở hoặc đang chạy
+     */
     public static List<Auction> getActiveAuctions() throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
         List<Auction> result = new ArrayList<>();
@@ -48,36 +59,43 @@ public class AuctionDAO {
         return result;
     }
 
+    /**
+     * Tạo mới một phiên đấu giá và lưu vào JSON
+     */
     public static void createAuction(Auction auction) throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
         auctions.add(auction);
 
+        // Chỉ sử dụng JSON để lưu trữ
         JsonUtil.saveToJson(DataManager.JSON_AUCTIONS, auctions);
-        SerializationUtil.serialize(DataManager.DAT_AUCTIONS, auctions);
 
-        LoggerUtil.info("Auction created: " + auction.getAuctionId());
+        LoggerUtil.info("Auction created and saved to JSON: " + auction.getAuctionId());
     }
 
+    /**
+     * Cập nhật thông tin phiên đấu giá hiện có
+     */
     public static void saveAuction(Auction auction) throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
 
+        // Xóa bản cũ, thêm bản mới cập nhật
         auctions.removeIf(a -> a.getAuctionId().equals(auction.getAuctionId()));
-
         auctions.add(auction);
 
         JsonUtil.saveToJson(DataManager.JSON_AUCTIONS, auctions);
-        SerializationUtil.serialize(DataManager.DAT_AUCTIONS, auctions);
 
-        LoggerUtil.info("Auction updated: " + auction.getAuctionId());
+        LoggerUtil.info("Auction updated in JSON: " + auction.getAuctionId());
     }
 
+    /**
+     * Xóa một phiên đấu giá khỏi hệ thống
+     */
     public static void deleteAuction(String auctionId) throws IOException, ClassNotFoundException {
         List<Auction> auctions = getAllAuctions();
         auctions.removeIf(a -> a.getAuctionId().equals(auctionId));
 
         JsonUtil.saveToJson(DataManager.JSON_AUCTIONS, auctions);
-        SerializationUtil.serialize(DataManager.DAT_AUCTIONS, auctions);
 
-        LoggerUtil.info("Auction deleted: " + auctionId);
+        LoggerUtil.info("Auction deleted from JSON: " + auctionId);
     }
 }
