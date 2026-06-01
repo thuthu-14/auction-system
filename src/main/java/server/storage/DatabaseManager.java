e7be0d6692bbb46d1222d77272e4920e27274ea1
thuthu-14 <thu07114@thu07114gmail.07114gmailcom>
2026-06-01 11:57:43 +0700

    commit lại version đúng

--- src/main/java/server/storage/DatabaseManager.java
+++ src/main/java/server/storage/DatabaseManager.java
@@ -7,6 +7,14 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static final String URL = "jdbc:sqlite:data/auction.db";
 
+    static {
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("SQLite JDBC driver not found", e);
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
